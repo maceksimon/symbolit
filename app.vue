890f06a7bf981d795830939d7bb2e7d38f041797
Šimon Macek <simon.macek@proficio.cz>
2024-02-11 18:21:05 -0500

    first commit

---
+++ app.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="max-w-xl mx-auto pt-24">
+    <label for="first-name" class="block text-sm font-medium leading-6 text-gray-50">Enter word</label>
+    <div class="flex flex-wrap gap-2 mt-2">
+      <input v-model="inputValue" type="text" name="first-name" id="first-name" autocomplete="given-name"
+        class="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+      <button type="button"
+        class="rounded bg-indigo-600 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        @click="word = inputValue">
+        Cusomize
+      </button>
+    </div>
+
+    <div v-if="word" class="flex gap-1 mt-16">
+      <div v-for="(character, index) in word" :key="`character-${index}`">
+        <CharacterDropdown :character="character" :position="index" @update:character="onUpdateCharacter" />
+      </div>
+    </div>
+
+    <div
+      class="flex flex-1 items-center justify-between truncate rounded-md border border-gray-700 bg-gray-800/50 backdrop-blur-lg mt-16">
+      <div class="flex-1 truncate px-4 py-3 text-sm">
+        <a href="#" class="text-2xl font-medium text-white hover:text-gray-200" @click.prevent="copy(output)">
+          {{ output }}
+        </a>
+      </div>
+      <div class="flex-shrink-0 flex items-center pr-2">
+        <template v-if="isSupported">
+          <a v-if="!copied" href="#" class="w-24 text-xs text-gray-300 hover:text-gray-400" @click.prevent="copy(output)">
+            Click to copy
+          </a>
+          <span v-if="copied" class="w-24 text-xs text-gray-300">
+            Copied!
+          </span>
+          <button type="button"
+            class="inline-flex h-8 w-8 items-center justify-center rounded-full bg-transparent text-gray-300 hover:text-gray-400 focus:border-none focus:outline-none focus:ring-2 focus:ring-gray-100/20 focus:ring-offset-0"
+            @click.prevent="copy(output)">
+            <!-- Icon: Copy -->
+            <svg v-show="!copied" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="h-5 w-5">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+            </svg>
+            <!-- Icon: Copied -->
+            <svg v-show="copied" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="h-5 w-5 text-green-300">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0 1 18 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3 1.5 1.5 3-3.75" />
+            </svg>
+          </button>
+        </template>
+        <template v-else>
+          <div class="inline-flex h-8 w-8 items-center justify-center rounded-full bg-transparent text-gray-300" disabled>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="h-5 w-5 text-gray-400">
+              <path stroke-linecap="round" stroke-linejoin="round"
+                d="M18.364 18.364A9 9 0 0 0 5.636 5.636m12.728 12.728A9 9 0 0 1 5.636 5.636m12.728 12.728L5.636 5.636" />
+            </svg>
+          </div>
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const inputValue = ref<string>('test')
+const word = ref<string>('test')
+const output = ref<string>('test')
+
+const onUpdateCharacter = (event: { index: number, character: string }) => {
+  output.value = word.value.substring(0, event.index) + event.character + word.value.substring(event.index + 1)
+}
+
+const { text, copy, copied, isSupported } = useClipboard({ source: output })
+</script>
+
+<style>
+body {
+  @apply bg-gray-800;
+}
+</style>
---
+++ components/CharacterDropdown.vue
@@ -0,0 +1,61 @@
+
+import type { mergeProps } from 'vue';
+<template>
+  <Menu as="div" class="relative inline-block text-left">
+    <div>
+      <MenuButton
+        class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+        {{ character }}
+      </MenuButton>
+    </div>
+
+    <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95"
+      enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75"
+      leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+      <MenuItems v-if="characterAliases.length"
+        class="absolute left-0 z-10 mt-2 w-56 origin-top-left rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <div v-for="alias in characterAliases" :key="alias" class="py-1">
+          <MenuItem v-slot="{ active }">
+          <a href="#" :class="[active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block px-4 py-2 text-sm']"
+            @click="onClickAlias(alias)">
+            {{ alias }}
+          </a>
+          </MenuItem>
+        </div>
+      </MenuItems>
+    </transition>
+  </Menu>
+</template>
+
+<script setup lang="ts">
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+
+const emits = defineEmits(['update:character'])
+
+const props = defineProps({
+  character: {
+    type: String,
+    required: true,
+  },
+  position: {
+    type: Number,
+    required: true,
+  }
+})
+
+const characterAliases = computed(() => {
+  switch (props.character) {
+    case 'A':
+      return ['𝐀', '𝐴', '𝑨', '𝒜', '𝓐', '𝔄', '𝔸']
+    default:
+      return []
+  }
+})
+
+const onClickAlias = (alias: string) => {
+  emits('update:character', {
+    index: props.position,
+    character: alias
+  })
+}
+</script>
